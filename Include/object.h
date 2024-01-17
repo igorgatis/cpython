c6e3892477863c264abb9f8c67de7df67735a134
Igor Gatis <igor.gatis@quintoandar.com.br>
2024-01-17 17:39:03 -0300

    wip

--- Include/object.h
+++ Include/object.h
@@ -801,9 +801,9 @@ static inline Py_ALWAYS_INLINE void Py_INCREF(PyObject *op)
         _Py_atomic_add_ssize(&op->ob_ref_shared, (1 << _Py_REF_SHARED_SHIFT));
     }
 #elif SIZEOF_VOID_P > 4
+    // Portable saturated add, branching on the carry flag and set low bits
     PY_UINT32_T cur_refcnt = op->ob_refcnt_split[PY_BIG_ENDIAN];
     PY_UINT32_T new_refcnt = cur_refcnt + 1;
-    // Portable saturated add, branching on the carry flag and set low bits
     #ifdef BRANCHELESS
     op->ob_refcnt_split[PY_BIG_ENDIAN] += (new_refcnt != 0);
     if (new_refcnt == 0) {
